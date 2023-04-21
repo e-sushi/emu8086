5cf3f43c8be4b8ff1889dce8430d934ac4968c39
sushi <officialaxos7@gmail.com>
2023-04-20 23:32:47 -0400

    get nearly all cases handled in listing 39

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(bad_style)] // really dumb
 #![allow(unused)] // rust seems to be unable to actually tell when something is used or unused
-use std::fs as filesystem;
+use std::{fs as filesystem, error::Error, fmt::format};
 use fstrings::*;
 use std::ops;
 
@@ -84,7 +84,7 @@ fn main() {
     // in a loop, but for loops don't allow that 
     let mut iter = buffer.iter();
     loop {
-        let byte = *{
+        let byte = *{ // TODO(sushi) we probably don't need to keep making stuff like byte2 and byte3, we just extract all the info we need from byte before continuing
             let next = iter.next();
             if next == None {break}
             next.unwrap()
@@ -95,38 +95,86 @@ fn main() {
             out += "mov ";
             let w   = hasbits!(byte 0b00001000);
             let reg = regtable[(w,getbits!(byte 0b00000111))];
-            out += &f!("{reg}, ");
             let value = if w {
-                let upper = *iter.next().expect("unexpected eof. wanted an upper byte for mov:imm->reg"); 
                 let lower = *iter.next().expect("unexpected eof. wanted a lower byte for mov:imm->reg");
+                let upper = *iter.next().expect("unexpected eof. wanted an upper byte for mov:imm->reg"); 
                 ((upper as u16) << 8) | (lower as u16)
             }else{
                 *iter.next().expect("unexpected eof. wanted immediate value for mov:imm->reg") as u16
             };
-            out += &f!("{value}");
+            out += &f!("{reg}, {value}");
         } else if hasbits!(byte 0b10001000) { // mov: register/memory to/from register
             out += "mov ";
-            let d = hasbits!(byte 0b00000010);
+            let d = hasbits!(byte 0b00000010); 
+            todo!(); // TODO(sushi) handle d 
             let w = hasbits!(byte 0b00000001);
             let byte2 = *iter.next().expect("unexpected eof. wanted byte 2 for mov:reg/mem<->reg");
             let mode = getbits!(byte2 0b11000000) >> 6;
             match mode {
-                0b11 => { // we are only dealing with registers 
+                0b00 => { // memory mode 
+                    // TODO(sushi) combine with following case
+                    let regl = regtable[(w, getbits!(byte2 0b00111000) >> 3)];
+                    let rm = getbits!(byte2 0b00000111);
+                    out += &f!("{regl}, ");
+                    out += &match rm {
+                        0b000 => f!("[bx + si]"),
+                        0b001 => f!("[bx + di]"),
+                        0b010 => f!("[bp + si]"),
+                        0b011 => f!("[bp + di]"),
+                        0b100 => f!("[si]"),
+                        0b101 => f!("[di]"),
+                        0b111 => f!("[bx]"),
+                        0b110 => {
+                            todo!();
+                        }
+                        _=>todo!()
+                    }
+                }
+                0b01 | 0b10 => { // memory mode, 8/16 bit displacement follows
+                    let regl = regtable[(w, getbits!(byte2 0b00111000) >> 3)];
+                    out += &f!("{regl}, ");
+                    let rm = getbits!(byte2 0b000000111);
+                    out += &match rm {
+                        0b000 => {"[bx + si"}
+                        0b001 => {"[bx + di"}
+                        0b010 => {"[bp + si"}
+                        0b011 => {"[bp + di"}
+                        0b100 => {"[si"}
+                        0b101 => {"[di"}
+                        0b110 => {"[bp"}
+                        0b111 => {"[bx"}
+                        _=>panic!()
+                    };
+                    if byte != 0 {
+                        out += &match mode {
+                            0b01 => {
+                                let byte3 = *iter.next().expect("unexpected eof. wanted byte 3 for mov:reg/mem<->reg");
+                                if byte3 != 0 {
+                                    format!(" + {}]", byte3.to_string())
+                                } else {
+                                    String::from("]")
+                                }
+                            }
+                            0b10 =>{
+                                let lower = *iter.next().expect("unexpected eof. wanted lower byte for mov:reg/mem<->reg");
+                                let upper = *iter.next().expect("unexpected eof. wanted upper byte for mov:reg/mem<->reg");
+                                let value = ((upper as u16) << 8) | (lower as u16);
+                                if value != 0 {
+                                    format!(" + {}]", value.to_string())
+                                } else {
+                                    String::from("]")
+                                }
+                            }
+                            _=>panic!()
+                        };
+                    } 
+                }
+                0b11 => { // register mode
                     let regl = regtable[(w,getbits!(byte2 0b00000111) >> 0)];
                     let regr = regtable[(w,getbits!(byte2 0b00111000) >> 3)];
                     out += &format!("{regl}, {regr}");
                 }
-                0b10 => {
-                    let regl = regtable[(w, getbits!(byte2 0b00111000) >> 3)];
-                    let rm = getbits!(byte2 0b00000111) >> 3;
-                    match rm {
-                        0b000 => {
-                            // RETURN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                        }
-                        _=>todo!()
-                    }
-
-                }
+                
                 _ => todo!()
             }
         } else if hasbits!(byte 0b11000110) { // mov: immediate to register/memory
