0dc09d9801d1aac8291be74da7f2b16a6a1e1b5b
sushi <officialaxos7@gmail.com>
2023-04-27 00:19:52 -0400

    finish listing 40

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![allow(bad_style)] // really dumb
 #![allow(unused)] // rust seems to be unable to actually tell when something is used or unused
-use std::{fs as filesystem, error::Error, fmt::format};
+use std::{fs as filesystem, error::Error, io};
 use fstrings::*;
-use std::ops;
+use tui;
+use crossterm;
+use trees;
 
 macro_rules! enum_display {
     ($enum_name:ident { $($variant_name:ident),+ $(,)? }) => {
@@ -64,14 +66,30 @@ const regtable : RegTable = RegTable{
 // in C
 macro_rules! hasbits { ($bytes:ident $value:expr) => { $bytes & $value == $value } }
 
+
+
 // just a bit mask with &, but using a macro is more consistent with 'hasbits'
 macro_rules! getbits { ($byte:ident $value:expr) => { $byte & $value } }
 
+macro_rules! get8bitdisp { ($iter:ident) => {*$iter.next().unwrap() as u16} }
+macro_rules! get16bitdisp { ($iter:ident) => {((*$iter.next().unwrap() as u16) | ((*$iter.next().unwrap() as u16) << 8))} }
+
+struct vec2{
+    x:f32,
+    y:f32
+}
+
+impl std::ops::Add<vec2> for vec2 {
+    type Output = vec2;
+    fn add(self, rhs:vec2) -> Self::Output{
+        vec2{x: self.x + rhs.x, y: self.y + rhs.y}
+    }
+}
 
 fn main() {
     let buffer = {
         // read file
-        let res = filesystem::read("data/listing_0039_more_movs");
+        let res = filesystem::read("data/listing_0040_challenge_movs");
         if res.is_err() {
             println!("could not open file due to: \"{}\"", res.err().unwrap());
             return;
@@ -79,6 +97,29 @@ fn main() {
         // get resulting vec if file is read successfully
         res.unwrap()
     };
+    
+    //ui();
+
+    const rmtable : [&str;8] = [
+        "[bx + si",
+        "[bx + di",
+        "[bp + si",
+        "[bp + di",
+        "[si",
+        "[di",
+        "[bp",
+        "[bx",
+    ];
+
+    fn get_value(w:bool, mut iter: &mut std::slice::Iter<u8>) -> u16 {
+        if w {
+            let lower = *iter.next().expect("unexpected eof. wanted a lower byte for mov:imm->reg") as u16;
+            let upper = *iter.next().expect("unexpected eof. wanted an upper byte for mov:imm->reg") as u16;
+            (upper << 8) | lower
+        } else {
+            *iter.next().expect("") as u16
+        }
+    }
 
     // use an iterator, because we may want to consume multiple bytes 
     // in a loop, but for loops don't allow that 
@@ -90,22 +131,15 @@ fn main() {
             next.unwrap()
         };
         let mut out = String::from("");
-
         if        hasbits!(byte 0b10110000) { // mov: immediate to register  
-            out += "mov ";
             let w   = hasbits!(byte 0b00001000);
-            let reg = regtable[(w,getbits!(byte 0b00000111))];
-            let value = if w {
-                let lower = *iter.next().expect("unexpected eof. wanted a lower byte for mov:imm->reg");
-                let upper = *iter.next().expect("unexpected eof. wanted an upper byte for mov:imm->reg"); 
-                ((upper as u16) << 8) | (lower as u16)
-            }else{
-                *iter.next().expect("unexpected eof. wanted immediate value for mov:imm->reg") as u16
-            };
-            out += &f!("{reg}, {value}");
+            out += &format!("mov {}, {}", 
+                regtable[(w, getbits!(byte 0b00000111))],
+                get_value(w, &mut iter,),
+            );
         } else if hasbits!(byte 0b10001000) { // mov: register/memory to/from register
             out += "mov ";
-            let d = hasbits!(byte 0b00000010); 
+            let d = hasbits!(byte 0b00000010);
             let w = hasbits!(byte 0b00000001);
             let byte2 = *iter.next().expect("unexpected eof. wanted byte 2 for mov:reg/mem<->reg");
             let mode = getbits!(byte2 0b11000000) >> 6;
@@ -114,37 +148,24 @@ fn main() {
                     let regl = regtable[(w, getbits!(byte2 0b00111000) >> 3)];
                     if d {out += &f!("{regl}, ");}
                     let rm = getbits!(byte2 0b000000111);
-                    out += &match rm {
-                        0b000 => {"[bx + si"}
-                        0b001 => {"[bx + di"}
-                        0b010 => {"[bp + si"}
-                        0b011 => {"[bp + di"}
-                        0b100 => {"[si"}
-                        0b101 => {"[di"}
-                        0b110 => {"[bp"}
-                        0b111 => {"[bx"}
-                        _=>panic!()
-                    };
-                    if byte != 0 && mode != 0b00 {
-                        match mode {
-                            0b01 => {
-                                let byte3 = *iter.next().expect("unexpected eof. wanted byte 3 for mov:reg/mem<->reg");
-                                if byte3 != 0 {
-                                    out += &format!(" + {}", byte3.to_string());
-                                } 
+                    // rust has NO way to break out of a match expr, so i guess we get to use an else here and nest everything
+                    // one more time for no reason!
+                    if mode == 0b00 && rm == 0b110 {
+                        out += &format!("[{}", get_value(w, &mut iter));
+                    } else {
+                        out += rmtable[rm as usize];
+                        if byte != 0 {
+                            let value = get_value(if mode == 0b01 {false} else if mode == 0b10 {true} else {panic!()}, &mut iter) as i16;
+                            if value != 0 {
+                                out += &if value < 0 {
+                                    format!(" - {}", value.abs())
+                                } else {
+                                    format!(" + {}", value)
+                                }
                             }
-                            0b10 =>{
-                                let lower = *iter.next().expect("unexpected eof. wanted lower byte for mov:reg/mem<->reg");
-                                let upper = *iter.next().expect("unexpected eof. wanted upper byte for mov:reg/mem<->reg");
-                                let value = ((upper as u16) << 8) | (lower as u16);
-                                if value != 0 {
-                                    out += &format!(" + {}", value.to_string());
-                                } 
-                            }
-                            _=>panic!()
-                        };
-                    } 
-                    out += &"]";
+                        } 
+                    }
+                    out += "]";
                     if !d {out += &f!(", {regl}");}
                 }
                 0b11 => { // register mode
@@ -152,19 +173,82 @@ fn main() {
                     let regr = regtable[(w,getbits!(byte2 0b00111000) >> 3)];
                     out += &format!("{regl}, {regr}");
                 }
-                
-                _ => todo!()
+                _ =>panic!()
             }
         } else if hasbits!(byte 0b11000110) { // mov: immediate to register/memory
-            todo!();
+            out += "mov ";
+            let w = hasbits!(byte 0b00000001);
+            let byte2 = *iter.next().expect("unexpected eof. wanted byte 2 for mov:imm->reg/mem");
+            let mode = getbits!(byte2 0b11000000) >> 6;
+            match mode {
+                0b00 => out += &format!("{}], {} {}", 
+                            rmtable[getbits!(byte2 0b00000111) as usize], 
+                            if w{"word"}else{"byte"}, 
+                            get_value(w,&mut iter)
+                        ),
+                0b01 => out += &format!("{}], {} {}", 
+                            rmtable[getbits!(byte2 0b00000111) as usize],
+                            if w {"word "}else{"byte "},
+                            get_value(w, &mut iter)
+                        ),
+                0b10 => out += &format!("{}], {} {} {}", // so silly, delle would love this
+                            rmtable[getbits!(byte2 0b00000111) as usize],
+                            {
+                                let value = get16bitdisp!(iter);
+                                if value != 0 {
+                                    format!(" + {}", value)
+                                } else {
+                                    String::from("")
+                                }
+                            },
+                            if w {", word "} else {", byte "},
+                            get_value(w, &mut iter)
+                        ),
+                _ =>panic!()
+            }
         } else if hasbits!(byte 0b10100000) { // mov: memory to acculator or accumulator to memory
-            todo!();
+            out += "mov ";
+            let w = hasbits!(byte 0b00000001);
+            let mta = hasbits!(byte 0b00000010);
+            if !mta { out += "ax, " } 
+            out += &format!("[{}]", if w {get16bitdisp!(iter)} else {get8bitdisp!(iter)});
+            if mta { out += ", ax" }
         } else if hasbits!(byte 0b10001100) { // mov: register/memory to segment register or vice versa
             todo!();
         }
 
 
+        
+
         println!("{out}");
+    }
+
+}
+
+fn ui() -> () {
+    
+    crossterm::terminal::enable_raw_mode().expect("crossterm backend failed to initialize");
+    
+    let mut stdout = std::io::stdout();
+    let mut backend = tui::backend::CrosstermBackend::new(std::io::stdout());
+    let mut terminal = tui::Terminal::new(backend).expect("tui terminal failed to initialize");
+
+    let mut frame = terminal.get_frame();
+
+    crossterm::execute!(
+        stdout, 
+        crossterm::terminal::EnterAlternateScreen, 
+        crossterm::event::EnableMouseCapture
+    );
+
+    {use tui::widgets::*;
+        terminal.draw(|f|{
+            let size = f.size();
+            let block = Block::default()
+                    .title("8086")
+                    .borders(Borders::ALL);
+            f.render_widget(block,size);
+        });
     }
 
 }
